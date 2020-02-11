a04d97d2739953ba42781e51af12a96efd886f2b
Shubham Kapoor <Shubham.Kapoor@arris.com>
2020-02-11 09:46:13 +0530

    Initial Commit

---
+++ GroceryAppTests/GroceryAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  GroceryAppTests.swift
+//  GroceryAppTests
+//
+//  Created by Shubham Kapoor on 11/02/20.
+//  Copyright © 2020 Shubham Kapoor. All rights reserved.
+//
+
+import XCTest
+@testable import GroceryApp
+
+class GroceryAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
